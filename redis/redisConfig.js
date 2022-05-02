547ec64375bf9ca29c9ede9d3ca3c4a7bf28a9d0
Prakash Acharya <pracharya2601@gmail.com>
2022-05-02 10:43:50 -0700

    clear redis config

--- redis/redisConfig.js
+++ redis/redisConfig.js
@@ -1,7 +1,7 @@
 const {createClient} = require('redis');
 module.exports = createClient(
     {
-        url: 'redis://:OdjhFDfIIuF5eODQ4H7ejU20cgPyJYTJ@SG-redish-testing-50990.servers.mongodirector.com:6379',
+        url: process.env.REDIS_URL,
         legacyMode: true
     }
 );
